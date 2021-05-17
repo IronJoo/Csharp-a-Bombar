01fff905a485124513c5048a27bb602974fa41bd
Paulo Ferro <paulo.ferro@novabase.pt>
2021-05-17 18:15:33 +0100

    Commit inicial

---
+++ GraphicDesigner/Poligono.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicDesigner
+{
+    public abstract class Poligono
+    {
+        public virtual int X { get; set; }
+        public virtual int Y { get; set; }
+
+        public abstract void Draw();
+    }
+
+    public class Retangulo : Poligono
+    {
+        public virtual int Height { get; set; }
+        public virtual int Bottom { get; set; }
+
+        public virtual int Width { get; set; }
+        public virtual int Right { get; set; }
+
+        public override void Draw()
+        {
+            Trace.WriteLine("Vou desenhar um retangulo");
+        }
+    }
+
+    public class Quadrado : Retangulo
+    {
+        public override int Height { get { return base.Height; } set { base.Height = value; base.Width = value; } }
+
+        public override int Width { get { return Height; } set { this.Height = value; } }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+            Trace.WriteLine("afinal foi um quadrado");
+        }
+    }
+
+    public class Circulo : Poligono
+    {
+        public int Raio { get; set; }
+
+        public int Diametro { get; set; }
+
+        public override void Draw()
+        {
+            Trace.WriteLine("Vou desenhar um circulo");
+        }
+    }
+}
---
+++ GraphicDesigner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace GraphicDesigner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Poligono> desenho = new List<Poligono>();
+
+            desenho.Add(new Retangulo() { X = 10, Y = 15 });
+            desenho.Add(new Retangulo() { X = 12, Y = 17 });
+
+            Retangulo q = new Quadrado() { X = 19, Y = 25, Height = 20 };
+            q.Height = 10;
+
+            Trace.WriteLine(((Quadrado)q).Height);
+
+            desenho.Add(q);
+
+            desenho.Add(new Circulo() { X = 14, Y = 23 });
+            desenho.Add(new Circulo() { X = 14, Y = 23 });
+            desenho.Add(new Circulo() { X = 14, Y = 23 });
+
+            foreach (var poligono in desenho)
+            {
+                poligono.Draw();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
